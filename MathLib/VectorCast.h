a38602471a34e675fb4faee3075d0f98e9fee863
Thomas Wiltshire <thomas.wiltshire14@icloud.com>
2017-05-09 15:25:57 +1000

    Fixed VectorCast Issues.

--- MathLib/VectorCast.h
+++ MathLib/VectorCast.h
@@ -8,7 +8,7 @@
 // Set up CastTo template.
 //--------------------------------------------------------------------------------------
 template <class D, class S>
-D CastTo(S source) {};
+static D CastTo(S source) {};
 
 //--------------------------------------------------------------------------------------
 // Cast a Vector2 to Vector3
@@ -19,7 +19,7 @@ D CastTo(S source) {};
 //		Returns a Vector3 from a Vector2.
 //--------------------------------------------------------------------------------------
 template <>
-Vector3 CastTo(Vector2 source) 
+static Vector3 CastTo(Vector2 source) 
 {
 	return Vector3(source.x, source.y, 0);
 };
@@ -33,7 +33,7 @@ Vector3 CastTo(Vector2 source)
 //		Returns a Vector2 from a Vector3.
 //--------------------------------------------------------------------------------------
 template <>
-Vector2 CastTo(Vector3 source)
+static Vector2 CastTo(Vector3 source)
 {
 	return Vector2(source.x, source.y);
 };
@@ -47,7 +47,7 @@ Vector2 CastTo(Vector3 source)
 //		Returns a Vector2 from a Vector4.
 //--------------------------------------------------------------------------------------
 template <>
-Vector2 CastTo(Vector4 source)
+static Vector2 CastTo(Vector4 source)
 {
 	return Vector2(source.x, source.y);
 };
@@ -61,7 +61,7 @@ Vector2 CastTo(Vector4 source)
 //		Returns a Vector4 from a Vector2.
 //--------------------------------------------------------------------------------------
 template <>
-Vector4 CastTo(Vector2 source)
+static Vector4 CastTo(Vector2 source)
 {
 	return Vector4(source.x, source.y, 0 , 0);
 };
@@ -75,7 +75,7 @@ Vector4 CastTo(Vector2 source)
 //		Returns a Vector3 from a Vector4.
 //--------------------------------------------------------------------------------------
 template <>
-Vector3 CastTo(Vector4 source)
+static Vector3 CastTo(Vector4 source)
 {
 	return Vector3(source.x, source.y, source.z);
 };
@@ -89,7 +89,7 @@ Vector3 CastTo(Vector4 source)
 //		Returns a Vector4 from a Vector3.
 //--------------------------------------------------------------------------------------
 template <>
-Vector4 CastTo(Vector3 source)
+static Vector4 CastTo(Vector3 source)
 {
 	return Vector4(source.x, source.y, source.z, 0);
 };
